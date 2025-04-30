b63b76bffa4138238a09f3b1e91c024148a835e3
Tore Martin Hagen <93583343+ToreMerkely@users.noreply.github.com>
2025-04-30 13:30:12 +0200

    OPS-42 Do not send paylod in GET. This fails in Jira API now (#128)

--- scripts/lib-jira.sh
+++ scripts/lib-jira.sh
@@ -15,30 +15,41 @@ function debug_log
 
 function loud_curl
 {
-  # curl that prints the server traceback if the response
-  # status code is not in the range 200-299
-  local -r method=$1; shift  # eg GET/POST
-  local -r url=$1; shift
-  local -r jsonPayload=$1; shift
-  local -r userArg=$1;shift
+    # curl that prints the server traceback if the response
+    # status code is not in the range 200-299
+    local -r method=$1; shift  # eg GET/POST
+    local -r url=$1; shift
+    local -r jsonPayload=$1; shift
+    local -r userArg=$1;shift
 
-  local -r outputFile=$(mktemp)
+    local -r outputFile=$(mktemp)
 
-  set +e
-  HTTP_CODE=$(curl --header 'Content-Type: application/json' \
-       --user "${userArg}" \
-       --output "${outputFile}" \
-       --write-out "%{http_code}" \
-       --request "${method}" \
-       --silent \
-       --data "${jsonPayload}" \
-       ${url})
+    set +e
+    if [ "${method}" == "GET" ]; then
+        HTTP_CODE=$(curl --header 'Content-Type: application/json' \
+            --user "${userArg}" \
+            --output "${outputFile}" \
+            --write-out "%{http_code}" \
+            --request "${method}" \
+            --silent \
+            ${url})
+    else
+        HTTP_CODE=$(curl --header 'Content-Type: application/json' \
+            --user "${userArg}" \
+            --output "${outputFile}" \
+            --write-out "%{http_code}" \
+            --request "${method}" \
+            --silent \
+            --data "${jsonPayload}" \
+            ${url})
+    fi
     set -e
+    
     if [[ ${HTTP_CODE} -lt 200 || ${HTTP_CODE} -gt 299 ]] ; then
-        >&2 cat ${outputFile}  # Request failed so send output to stderr
-        >&2 echo
-        rm ${outputFile}
-        exit 2
+    >&2 cat ${outputFile}  # Request failed so send output to stderr
+    >&2 echo
+    rm ${outputFile}
+    exit 2
     fi
     cat ${outputFile}  # Correct response send to stdout
     echo
